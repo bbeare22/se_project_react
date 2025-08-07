735ff3fa5d24d065be15f5ddb01fd9dcb9979877
BrettBeare <beare.logan@yahoo.com>
2025-08-06 19:51:41 -0600

    Project 15 Update 2

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,12 +1,12 @@
 import { checkResponse } from "./checkResponse";
 
-const BASE_URL =
+const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://api.weatherapp.jumpingcrab.com"
     : "http://localhost:3001";
 
 export const signup = ({ name, avatar, email, password }) => {
-  return fetch(`${BASE_URL}/signup`, {
+  return fetch(`${baseUrl}/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -16,7 +16,7 @@ export const signup = ({ name, avatar, email, password }) => {
 };
 
 export const signin = ({ email, password }) => {
-  return fetch(`${BASE_URL}/signin`, {
+  return fetch(`${baseUrl}/signin`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -26,7 +26,7 @@ export const signin = ({ email, password }) => {
 };
 
 export const checkToken = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
+  return fetch(`${baseUrl}/users/me`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -36,7 +36,7 @@ export const checkToken = (token) => {
 };
 
 export const updateProfile = ({ name, avatar }, token) => {
-  return fetch(`${BASE_URL}/users/me`, {
+  return fetch(`${baseUrl}/users/me`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
